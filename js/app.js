e4c3b86cf94c6e12a5678239590067741311eb2c
nlin <nlin@thoughtworks.com>
2018-05-08 21:56:11 +0800

    refactor with api v1

--- js/app.js
+++ js/app.js
@@ -1,74 +1,74 @@
-
 (function($, owner) {
-	
+
 	/**
 	 * 项目的基本配置 
 	 */
 	owner.config = {
-//		baseUrl:"http://www.1008691.club/"
-		baseUrl:"http://admin.tengdakey.com/"
-//		baseUrl:"http://localhost/tinda_admin/"
+		//		baseUrl:"http://www.1008691.club/"
+		baseUrl: "http://admin.tengdakey.com/"
+//				baseUrl:"http://localhost/tinda_admin/"
 	}
 	/**
 	 * 用户登录
 	 **/
 	owner.login = function(loginInfo, callback) {
-		
-		if (plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
-        mui.toast("网络异常，请检查网络设置！");
- 
-       } else {
-		
-		
-		
-		
-		callback = callback || $.noop;
-		loginInfo = loginInfo || {};
-		loginInfo.account = loginInfo.account || '';
-		loginInfo.password = loginInfo.password || '';
-		loginInfo.usertoke = loginInfo.usertoke || localStorage.getItem('usertoke');
-		if (loginInfo.account.length < 1) {
-			return callback('请输入账号！');
-		}
-		if (loginInfo.password.length < 1) {
-			return callback('请输入密码！');
-		}
-		
-		if(!loginInfo.usertoke){
-			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-			for( var i=0; i < 7; i++ )
-				loginInfo.usertoke += possible.charAt(Math.floor(Math.random() * possible.length));
-		}
- 
-		/////
-		mui.init();
-		
-		mui.post(this.config.baseUrl + 'td2017.php',{ name:loginInfo.account,psw:loginInfo.password,ustoke:loginInfo.usertoke},//请求，地址是服务器本机的ip地址//要传送的数据
-	    function(data){ //服务器返回数据回来时应该做什么的函数
-	   if(data=='1'){
-	     localStorage.setItem('username',loginInfo.account);
-	     localStorage.setItem('usertoke',loginInfo.usertoke);
-	     
-	     var username=JSON.stringify({"name":loginInfo.account,"toke":loginInfo.usertoke});
-	         localStorage.setItem("userzl",username);
-	     return owner.createState(loginInfo.account,loginInfo.usertoke, callback);
-		 }
-	   if(data=='2'){
-	     return callback('帐号已过期，请联系服务商！');
-		 }
-	   if(data=='3'){
-		 return callback('用户名或密码错误');
-		 }
-	   	   if(data=='0'){
-		 return callback('帐号不存在！');
-		 }},
-		 'text'
-		 );
-		/////
-		}
-	};
 
-	owner.createState = function(name,usertoke, callback) {
+		if(plus.networkinfo.getCurrentType() == plus.networkinfo.CONNECTION_NONE) {
+			mui.toast("网络异常，请检查网络设置！");
+
+		} else {
+
+			callback = callback || $.noop;
+			loginInfo = loginInfo || {};
+			loginInfo.account = loginInfo.account || '';
+			loginInfo.password = loginInfo.password || '';
+			loginInfo.usertoke = loginInfo.usertoke || localStorage.getItem('usertoke');
+			if(loginInfo.account.length < 1) {
+				return callback('请输入账号！');
+			}
+			if(loginInfo.password.length < 1) {
+				return callback('请输入密码！');
+			}
+
+			if(!loginInfo.usertoke) {
+				var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+				for(var i = 0; i < 7; i++)
+					loginInfo.usertoke += possible.charAt(Math.floor(Math.random() * possible.length));
+			}
+
+			/////
+			mui.init();
+			mui.post(this.config.baseUrl + 'api/v1/login', {
+					username: loginInfo.account,
+					password: loginInfo.password,
+					token: loginInfo.usertoke
+				},
+				function(response) {
+					response = JSON.parse(response);
+					
+					if(response.status == 200) {
+						localStorage.setItem('username', loginInfo.account);
+						localStorage.setItem('usertoke', loginInfo.usertoke);
+
+						var userInfoString = JSON.stringify({
+							"name": loginInfo.account,
+							"toke": loginInfo.usertoke
+						});
+						localStorage.setItem("userzl", userInfoString);
+						localStorage.setItem("authToken", response.data.token);
+						localStorage.setItem('permission', JSON.stringify(response.data.permission));
+
+						owner.createState(loginInfo.account, loginInfo.usertoke, callback);
+						return;
+					} else {
+						return callback('用户名或密码错误');
+					}
+				}
+			);
+		}
+	}
+
+	owner.createState = function(name, usertoke, callback) {
 		var state = owner.getState();
 		state.account = name;
 		state.token = usertoke;
@@ -84,13 +84,13 @@
 		regInfo = regInfo || {};
 		regInfo.account = regInfo.account || '';
 		regInfo.password = regInfo.password || '';
-		if (regInfo.account.length < 5) {
+		if(regInfo.account.length < 5) {
 			return callback('用户名最短需要 5 个字符');
 		}
-		if (regInfo.password.length < 6) {
+		if(regInfo.password.length < 6) {
 			return callback('密码最短需要 6 个字符');
 		}
-		if (!checkEmail(regInfo.email)) {
+		if(!checkEmail(regInfo.email)) {
 			return callback('邮箱地址不合法');
 		}
 		var users = JSON.parse(localStorage.getItem('$users') || '[]');
@@ -120,20 +120,13 @@
 
 	var checkEmail = function(email) {
 		email = email || '';
-		return (email.length > 3 && email.indexOf('@') > -1);
+		return(email.length > 3 && email.indexOf('@') > -1);
 	};
 
-	/**
-	 * 找回密码
-	 **/
-	/*owner.forgetPassword = function(email, callback) {
-		callback = callback || $.noop;
-		if (!checkEmail(email)) {
-			return callback('邮箱地址不合法');
-		}
-		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
-
-	};*/
+	owner.wrapTokenForUrl = function(path) {
+		var token = localStorage.getItem('authToken');
+		return path + "?token=" + token
+	}
 
 	/**
 	 * 获取应用本地配置
@@ -147,17 +140,17 @@
 	 * 设置应用本地配置
 	 **/
 	owner.getSettings = function() {
-			var settingsText = localStorage.getItem('$settings') || "{}";
-			return JSON.parse(settingsText);
-		}
-		/**
-		 * 获取本地是否安装客户端
-		 **/
+		var settingsText = localStorage.getItem('$settings') || "{}";
+		return JSON.parse(settingsText);
+	}
+	/**
+	 * 获取本地是否安装客户端
+	 **/
 	owner.isInstalled = function(id) {
-		if (id === 'qihoo' && mui.os.plus) {
+		if(id === 'qihoo' && mui.os.plus) {
 			return true;
 		}
-		if (mui.os.android) {
+		if(mui.os.android) {
 			var main = plus.android.runtimeMainActivity();
 			var packageManager = main.getPackageManager();
 			var PackageManager = plus.android.importClass(packageManager)
@@ -168,9 +161,9 @@
 			}
 			try {
 				return packageManager.getPackageInfo(packageName[id], PackageManager.GET_ACTIVITIES);
-			} catch (e) {}
+			} catch(e) {}
 		} else {
-			switch (id) {
+			switch(id) {
 				case "qq":
 					var TencentOAuth = plus.ios.import("TencentOAuth");
 					return TencentOAuth.iphoneQQInstalled();
@@ -185,72 +178,127 @@
 			}
 		}
 	}
-	
-	
+
 }(mui, window.app = {}));
 
 /**
  * 日期格式化 
  */
-Date.prototype.Format = function (fmt) { //author: meizz 
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+Date.prototype.Format = function(fmt) { //author: meizz 
+	var o = {
+		"M+": this.getMonth() + 1, //月份 
+		"d+": this.getDate(), //日 
+		"h+": this.getHours(), //小时 
+		"m+": this.getMinutes(), //分 
+		"s+": this.getSeconds(), //秒 
+		"q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+		"S": this.getMilliseconds() //毫秒 
+	};
+	if(/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for(var k in o)
+		if(new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
 }
 
-function homeA(){
-    mui.openWindow({
-    url:'main.html',
-    id:'main',
-    });
+Array.prototype.contain = function(val) {
+	for(var i = 0; i < this.length; i++) {
+		if(this[i] == val) {
+			return true;
+		}
+	}
+	return false;
+};
+
+function checkPermission(feature) {
+	var features = JSON.parse(localStorage.getItem("permission"));
+	if(!features.contain(feature)) {
+		mui.toast("请到注册页面自助开通VIP账户！");
+		return false;
+	}
+	return true;
 }
 
-function rj(){
-    mui.openWindow({
-    url:'rj.html',
-    id:'rj',
-    });
+function zlcxA() {
+	if(!checkPermission('zlcx')) {
+		return;
+	}
+
+	mui.openWindow({
+		url: 'zlcx.html',
+		id: 'zlcx',
+	});
 }
 
+function passcxA() {
+	if(!checkPermission('passcx')) {
+		return;
+	}
 
-function zlcxA(){
-    mui.openWindow({
-    url:'zlcx.html',
-    id:'zlcx',
-    });
-}
-function passcxA(){
-    mui.openWindow({
-    url:'passcx.html',
-    id:'passcx',
-    });
-}
-function settingA(){
-    mui.openWindow({
-    url:'setting.html',
-    id:'setting',
-    });
-}
-function aboutd(){
-    mui.openWindow({
-    url:'daip.html?id=about',
-    id:'about',
-    });
+	mui.openWindow({
+		url: 'passcx.html',
+		id: 'passcx',
+	});
 }
 
-function redirectToLives(){
-		    mui.openWindow({
-    url:'lives.html',
-    id:'lives',
-    });
+function homeA() {
+	mui.openWindow({
+		url: 'main.html',
+		id: 'main',
+	});
+}
+
+function rj() {
+	mui.openWindow({
+		url: 'rj.html',
+		id: 'rj',
+	});
+}
+
+function settingA() {
+	mui.openWindow({
+		url: 'setting.html',
+		id: 'setting',
+	});
+}
+
+function aboutd() {
+	mui.openWindow({
+		url: 'daip.html?id=about',
+		id: 'about',
+	});
+}
+
+function redirectToLives() {
+	mui.openWindow({
+		url: 'lives.html',
+		id: 'lives',
+	});
+}
+
+function fwlx() {
+	mui.openWindow({
+		url: 'daip.html?id=fwlx',
+		id: 'fwlx',
+	});
+}
+
+function jxzc() {
+	mui.openWindow({
+		url: 'daip.html?id=jxzc',
+		id: 'jxzc',
+	});
+}
+
+function personal() {
+	mui.openWindow({
+		url: 'personal.html',
+		id: 'personal',
+	});
+}
+
+function gx() {
+	mui.openWindow({
+		url: 'gx.html',
+		id: 'gx',
+	});
 }
--- js/appuser.js
+++ js/appuser.js
@@ -1,45 +1,47 @@
 setTimeout("alus1()");
-function alus1() { 
- 	//远程提交
+
+function alus1() {
+	//远程提交
 	var userszl = JSON.parse(localStorage.getItem("userzl"));
-	if(userszl==null){
-		setTimeout("alus1()",10000);//10秒后再次执行
+	if(userszl == null) {
+		setTimeout("alus1()", 10000); //10秒后再次执行
 		return;
-	}else{
-		mui.ajax(app.config.baseUrl + 'usersyanz.php',{
-			data:{
-				username1:userszl.name,
-				usertoke1:userszl.toke
+	} else {
+		mui.ajax(app.config.baseUrl + app.wrapTokenForUrl('api/v1/online'), {
+			data: {
+				token: userszl.toke
 			},
-			dataType:'json',
-			type:'post',
-			timeout:10000,         
-			success:function(data){
-				if(data=='1'){
-						setTimeout("alus1()",10000);//10秒后再次执行
-					 	return;
-			      }else if(data=='2'){
-			                mui.toast('你的帐号已在另处登录！');
-			                localStorage.clear();
-			                app.setState({});
-			                plus.webview.getLaunchWebview().show("pop-in");
-			                setTimeout("alus1()",10000);//10秒后再次执行
-			                return;
-			       }else if(data=='3'){
-			               mui.toast('帐号已过期，请联系服务商！');
-			                localStorage.clear();
-			                app.setState({});
-					        plus.webview.getLaunchWebview().show("pop-in");
-			                setTimeout("alus1()",10000);//10秒后再次执行
-					        return;
-			       }
-			       setTimeout("alus1()",10000);//10秒后再次执行
+			dataType: 'json',
+			type: 'post',
+			timeout: 10000,
+			success: function(data) {
+				if(data.status == 200) {
+					setTimeout("alus1()", 10000); //10秒后再次执行
 					return;
+				} else if(data.status == 403) {
+					// token 过期 或者 身份丢失
+					localStorage.clear();
+					app.setState({});
+					plus.webview.getLaunchWebview().show("pop-in");
+					setTimeout("alus1()", 10000); //10秒后再次执行
+					return;
+				} else {
+					// client token 不一致，被T下线
+					mui.toast('你的帐号已在另处登录！');
+					localStorage.clear();
+					app.setState({});
+					plus.webview.getLaunchWebview().show("pop-in");
+					setTimeout("alus1()", 10000); //10秒后再次执行
+					return;
+				}
+				setTimeout("alus1()", 10000); //10秒后再次执行
+				return;
 			},
-			error:function(xhr,type,errorThrown){
-				setTimeout("alus1()",10000);//10秒后再次执行
+			error: function(xhr, type, errorThrown) {
+				setTimeout("alus1()", 10000); //10秒后再次执行
 				return;
 			}
+
 		});
-}
+	}
 }
